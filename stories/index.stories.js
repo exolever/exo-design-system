6ffae5e05ed1d0212b16928f3bc87c6a14c19db1
Alex King <alex@exolever.com>
2018-09-26 10:41:21 +0900

    Basic Story - with string

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -3,3 +3,5 @@ import { storiesOf } from '@storybook/html';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
+storiesOf('Welcome', module).add('to Storybook', () => '<button>Text</button>');
+

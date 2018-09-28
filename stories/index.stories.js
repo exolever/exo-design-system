ae21455d72a9c3d07b70f482488e441cb841a4fa
Alex King <alex@exolever.com>
2018-09-28 11:37:33 +0900

    Cleaner start for storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,7 +1,15 @@
-
+import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+// import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+import { action, withActions } from '@storybook/addon-actions';
+import '@material/mwc-button';
 
-storiesOf('Welcome', module).add('to Storybook', () => '<button>Text</button>');
-
+storiesOf('Demo', module)
+  .add('heading', () => '<h1>Hello World</h1>')
+  .add('button', () => {
+    const button = document.createElement('mwc-button');
+    button.type = 'button';
+    button.innerText = 'Hello Button';
+    button.addEventListener('click', e => action('clicked')());
+    return button;
+  });

d2384f2255ea98134cb3a85a2f132b7597675924
Alex King <alex@exolever.com>
2018-10-01 09:41:08 +0900

    Restrucuting for typescript

--- stories/0-intro.stories.js
+++ stories/0-intro.stories.js
@@ -1,8 +1,6 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
-// import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import { action, withActions } from '@storybook/addon-actions';
-import '@material/mwc-button';
 
 storiesOf('Welcome', module)
   .add('heading', () => '<h1>Hello World</h1>')
--- stories/button.stories.js
+++ stories/button.stories.js
@@ -2,13 +2,20 @@ import { document, console } from 'global';
 import { storiesOf, addDecorator } from '@storybook/html';
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import { knobToggleAttr, bindEvent } from '../storybook-utils/knobUtils';
-import '@material/mwc-button';
+import {html} from '@polymer/lit-element/lit-element.js';
+import { Button } from 'material-components-web-components/packages/button';
+class CustomButton extends Button {
+  _renderStyle(){
+    return html`${super._renderStyle()}<style>.mdc-button{border-radius: 30px}</style>`;
+  }
+}
+customElements.define('custom-button', CustomButton);
 
 
 const buttonStories = storiesOf('Buttons', module);
 buttonStories.addDecorator(withKnobs);
 buttonStories.add('text', () => {
-    const button = document.createElement('mwc-button');
+    const button = document.createElement('custom-button');
     button.setAttribute('Label', text('Button Label', 'Hello world'));
     knobToggleAttr(button, boolean('disabled', false), 'disabled');
     knobToggleAttr(button, boolean('rasied', false), 'raised');

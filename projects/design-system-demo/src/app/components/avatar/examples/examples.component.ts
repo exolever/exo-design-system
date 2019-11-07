25e9753fd787f690cbf0473ff9e198e2372c6720
Nacho <nacho@openexo.com>
2019-11-07 12:20:04 +0100

    #EXO-4812 New certificate codes

--- projects/design-system-demo/src/app/components/avatar/examples/examples.component.ts
+++ projects/design-system-demo/src/app/components/avatar/examples/examples.component.ts
@@ -1,37 +1,68 @@
 import { Component } from '@angular/core';
 
+enum CertificateEnum {
+  FOUNDATION = 'CFO',
+  CONSULTANT = 'CCO',
+  SPRINT_COACH = 'CSC',
+  AMBASSADOR = 'CEA',
+  TRAINER = 'CTR',
+  BOARD_ADVISOR = 'CBA',
+}
+
 @Component({
   templateUrl: './examples.component.html',
   styleUrls: ['./examples.component.scss'],
 })
 export class ExamplesComponent {
 
-  certifications5 = [
-    { name: 'ExO Coach', code: 'X' },
-    { name: 'ExO Ambassador', code: 'K' },
-    { name: 'ExO Trainer', code: 'TR' },
-    { name: 'ExO Fundations', code: 'EF' },
+  certifications1 = [
+    { name: 'ExO Board Advisor', code: CertificateEnum.BOARD_ADVISOR },
+  ];
+
+  certifications2 = [
+    { name: 'ExO Board Advisor', code: CertificateEnum.BOARD_ADVISOR },
+    { name: 'ExO Consultants', code: CertificateEnum.CONSULTANT },
+  ];
+
+  certifications3 = [
+    { name: 'ExO Coach', code: CertificateEnum.SPRINT_COACH },
+    { name: 'ExO Ambassador', code: CertificateEnum.AMBASSADOR },
+    { name: 'ExO Trainer', code: CertificateEnum.TRAINER },
   ];
 
   certifications4 = [
-    { name: 'ExO Coach', code: 'X' },
-    { name: 'ExO Ambassador', code: 'K' },
-    { name: 'ExO Trainer', code: 'TR' },
+    { name: 'ExO Coach', code: CertificateEnum.SPRINT_COACH },
+    { name: 'ExO Ambassador', code: CertificateEnum.AMBASSADOR },
+    { name: 'ExO Trainer', code: CertificateEnum.TRAINER },
+    { name: 'ExO Foundations', code: CertificateEnum.FOUNDATION },
+  ];
+
+  certifications5 = [
+    { name: 'ExO Coach', code: CertificateEnum.SPRINT_COACH },
+    { name: 'ExO Ambassador', code: CertificateEnum.AMBASSADOR },
+    { name: 'ExO Trainer', code: CertificateEnum.TRAINER },
+    { name: 'ExO Board Advisor', code: CertificateEnum.BOARD_ADVISOR },
+    { name: 'ExO Foundations', code: CertificateEnum.FOUNDATION },
   ];
 
   certifications6 = [
-    { name: 'ExO Coach', code: 'X' },
-    { name: 'ExO Ambassador', code: 'K' },
-    { name: 'ExO Trainer', code: 'TR' },
-    { name: 'ExO Board Advisor', code: 'BA' },
-    { name: 'ExO Consultants', code: 'EC' },
-    { name: 'ExO Foundations', code: 'EF' },
+    { name: 'ExO Coach', code: CertificateEnum.SPRINT_COACH },
+    { name: 'ExO Ambassador', code: CertificateEnum.AMBASSADOR },
+    { name: 'ExO Trainer', code: CertificateEnum.TRAINER },
+    { name: 'ExO Board Advisor', code: CertificateEnum.BOARD_ADVISOR },
+    { name: 'ExO Consultants', code: CertificateEnum.CONSULTANT },
+    { name: 'ExO Foundations', code: CertificateEnum.FOUNDATION },
   ];
+
   imagesSet = [
     { src: null, tooltip: 'Richard Nixon' },
+    // tslint:disable-next-line:max-line-length
     { src: 'https://vignette.wikia.nocookie.net/en.futurama/images/f/f2/PhilipJ.Fry.png/revision/latest/top-crop/width/200/height/150?cb=20110916120042', tooltip: 'Philip J. Fry' },
+    // tslint:disable-next-line:max-line-length
     { src: 'https://vignette.wikia.nocookie.net/en.futurama/images/d/d4/Turanga_Leela.png/revision/latest/top-crop/width/200/height/150?cb=20150218013044', tooltip: 'Turanga Leela' },
+    // tslint:disable-next-line:max-line-length
     { src: 'https://vignette.wikia.nocookie.net/en.futurama/images/4/43/Bender.png/revision/latest/top-crop/width/200/height/150?cb=20150206072725', tooltip: 'Bender Bending Rodriguez' },
+    // tslint:disable-next-line:max-line-length
     { src: 'https://vignette.wikia.nocookie.net/en.futurama/images/9/94/AmyWong.png/revision/latest/top-crop/width/200/height/150?cb=20170123190854', tooltip: 'Amy Kroker' },
   ];
 
